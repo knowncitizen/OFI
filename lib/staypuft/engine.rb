836d2f0cc5eed75122f6874385267281c8c2604f
Imre Farkas <ifarkas@redhat.com>
2014-09-02 11:01:59 +0200

    Add Fencing support
    ...by overriding Foreman views and controllers.
    The following files are copied over from Foreman and modified for the
    specific use-case:
    - app/assets/javascripts/staypuft/host_edit.js
    - app/views/hosts/_form.html.erb
    - app/views/hosts/_unattended.html.erb

--- lib/staypuft/engine.rb
+++ lib/staypuft/engine.rb
@@ -38,6 +38,7 @@ module Staypuft
       ::Hostgroup.send :include, Staypuft::Concerns::HostgroupExtensions
       ::Environment.send :include, Staypuft::Concerns::EnvironmentExtensions
       ::LookupKey.send :include, Staypuft::Concerns::LookupKeyExtensions
+      ::Nic::Base.send :include, Staypuft::Concerns::NicFencingExtensions
 
       # preload all the Foreman's lib files but only in production
       if Rails.env.production?
@@ -47,6 +48,11 @@ module Staypuft
             sort_by { |v| v.scan('/').size }. # ordered by the directory depth
             map { |v| require_dependency v }
       end
+
+      ::HostsController.view_paths.paths
+      staypuft_view_path = ::HostsController.view_paths.paths.find { |view_path| view_path.to_path =~ /\/staypuft\//}
+      ::HostsController.view_paths.paths.delete(staypuft_view_path)
+      ::HostsController.view_paths.paths.prepend(staypuft_view_path)
     end
 
     rake_tasks do
